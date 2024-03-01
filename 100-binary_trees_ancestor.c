cddda83b6963d29f930abcd75ed6c5600f86f9a4
Eltrone <amineelorche@gmail.com>
2024-02-29 17:08:47 -0800

    file added

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -3,22 +3,22 @@
 static int path_to_root(const binary_tree_t *root, const binary_tree_t *node)
 {
 	if (!root)
-		return 0;
+		return (0);
 	if (root == node)
-		return 1;
-	return path_to_root(root->parent, node);
+		return (1);
+	return (path_to_root(root->parent, node));
 }
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	if (!first || !second)
-		return NULL;
+		return (NULL);
 
 	if (path_to_root(second, first))
-		return (binary_tree_t *)first;
+		return ((binary_tree_t *)first);
 
 	if (path_to_root(first, second))
-		return (binary_tree_t *)second;
+		return ((binary_tree_t *)second);
 
-	return binary_trees_ancestor(first->parent, second);
+	return (binary_trees_ancestor(first->parent, second));
 }
