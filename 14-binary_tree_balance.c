596ac237d4f00cedce19c8bf74aa1afe1790549e
Eltrone <amineelorche@gmail.com>
2024-02-29 16:01:44 -0800

    file added

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,13 +25,14 @@ int binary_tree_balance(const binary_tree_t *tree)
  *
  * Return: The height of the tree. If tree is NULL, return 0.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+        return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+    size_t left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+    size_t right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (left_height > right_height ? left_height + 1 : right_height + 1);
+    return (left_height > right_height ? left_height : right_height);
 }
